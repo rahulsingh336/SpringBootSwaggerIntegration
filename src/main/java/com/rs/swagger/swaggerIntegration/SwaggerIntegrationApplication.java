3b8d70408077365eedc40892fdde50194a2cfab2
EZN1P8L <rahul.singh@volkswagen.co.in>
2017-08-21 22:40:18 +0530

    Initial Commit

---
+++ src/main/java/com/rs/swagger/swaggerIntegration/SwaggerIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.rs.swagger.swaggerIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerIntegrationApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rs/swagger/swaggerIntegration/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.rs.swagger.swaggerIntegration.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rs.swagger.swaggerIntegration.dto.Greeting;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
